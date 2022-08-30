9131a0ae79ada7e05d88c21d1b68c751a043007b
Will <slorkda@gmail.com>
2022-08-29 21:05:05 -0700

    Added mean, median, and mode functions

--- APStats/General.hs
+++ APStats/General.hs
@@ -1,1 +1,29 @@
 module APStats.General where
+
+import Data.List
+
+appearencesTable :: Ord a => [a] -> [(a, Int)]
+appearencesTable = ((map (\full@(first:_) -> (first, length full))) . group) . sort
+
+appearencesTable_Ordering :: Ord b => (a, b) -> (a, b) -> Ordering
+appearencesTable_Ordering (_, a) (_, b)
+    | a < b = GT
+    | a > b = LT
+    | otherwise = EQ
+
+mean :: (Fractional a, Foldable t) => t a -> a
+mean x = (sum x) / (fromIntegral (length x))
+
+median :: (Ord a, Fractional a) => [a] -> a
+median xs
+    | null xs = 0
+    | odd len = sortedList !! mid
+    | otherwise = evenMedian
+    where
+        evenMedian = ((sortedList !! mid) + (sortedList !! (mid - 1))) / 2
+        len = length xs
+        mid = len `div` 2
+        sortedList = sort xs
+
+mode :: Ord a => [a] -> a
+mode x = fst $ head  $ sortBy appearencesTable_Ordering (appearencesTable x)
--- apstats.hs
+++ apstats.hs
@@ -4,8 +4,9 @@ import APStats.General
 
 -- This is a wrapper for the functions used in the APStats library
 
-small_list :: [Int]
+larger_list = [1, 10, 100, 1000]
+pairs = [(0, 1), (2, 3)]
 small_list = [3, 3, 2, 1, 1, 1, 77]
 
 main :: IO ()
-main = print (small_list)
+main = print (median small_list)
