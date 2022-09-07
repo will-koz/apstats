2a6d5aa44d87ad4a93af3116936c67357e334d72
Will <slorkda@gmail.com>
2022-09-06 17:33:57 -0700

    Added sample word list

--- apstats.hs
+++ apstats.hs
@@ -7,6 +7,7 @@ import APStats.General
 larger_list = [1, 10, 100, 1000]
 pairs = [(0, 1), (2, 3)]
 small_list = [3, 3, 2, 1, 1, 1, 77]
+wordlist = ["Hi", "my", "name", "is", "Hal"]
 
 main :: IO ()
 main = print (median small_list)
