1d4d80db903cfa6d616c4d76971d4ef908c0d1af
Will <slorkda@gmail.com>
2022-09-06 17:36:59 -0700

    Working on mean_list

--- APStats/General.hs
+++ APStats/General.hs
@@ -14,6 +14,9 @@ appearencesTable_Ordering (_, a) (_, b)
 mean :: (Fractional a, Foldable t) => t a -> a
 mean x = (sum x) / (fromIntegral (length x))
 
+mean_list :: [[a]] -> Int
+mean_list x = mean (map length x)
+
 median :: (Ord a, Fractional a) => [a] -> a
 median xs
     | null xs = 0
@@ -28,13 +31,13 @@ median xs
 mode :: Ord a => [a] -> a
 mode x = fst $ head  $ sortBy appearencesTable_Ordering (appearencesTable x)
 
-ntile :: Int -> [a] -> Maybe [a]
-ntile x ys = take x sys
-    where
-        sys = sort ys
-
-percentile :: [a] -> Maybe [a]
-percentile = ntile 100 -- I don't expect this to be used very often
-
-quartile :: [a] -> Maybe [a]
-quartile = ntile 4
+-- ntile :: Int -> [a] -> Maybe [a]
+-- ntile x ys = take x sys
+--     where
+--         sys = sort ys
+--
+-- percentile :: [a] -> Maybe [a]
+-- percentile = ntile 100 -- I don't expect this to be used very often
+--
+-- quartile :: [a] -> Maybe [a]
+-- quartile = ntile 4
