fea99c35ea42a19c7b23f562eed4c36499aec78c
Will <slorkda@gmail.com>
2022-09-07 19:47:12 -0700

    Uncommented ntile

--- APStats/General.hs
+++ APStats/General.hs
@@ -38,10 +38,10 @@ median xs
 mode :: Ord a => [a] -> a
 mode x = fst $ head  $ sortBy appearencesTable_Ordering (appearencesTable x)
 
--- ntile :: Int -> [a] -> Maybe [a]
--- ntile x ys = take x sys
---     where
---         sys = sort ys
+ntile :: (Ord a) => Int -> [a] -> Maybe [a]
+ntile x ys = Just (take x sys)
+    where
+        sys = sort ys
 --
 -- percentile :: [a] -> Maybe [a]
 -- percentile = ntile 100 -- I don't expect this to be used very often
